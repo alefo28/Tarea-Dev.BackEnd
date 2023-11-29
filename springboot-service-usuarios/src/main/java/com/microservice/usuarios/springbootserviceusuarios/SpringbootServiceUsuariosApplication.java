fff9f7953483f1b3788688dba4ace13ce918e4b2
alefo28 <alefo28@gmail.com>
2023-11-28 19:38:50 -0600

    Creando Servicio Usuarios

---
+++ springboot-service-usuarios/src/main/java/com/microservice/usuarios/springbootserviceusuarios/SpringbootServiceUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.usuarios.springbootserviceusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServiceUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiceUsuariosApplication.class, args);
+	}
+
+}
