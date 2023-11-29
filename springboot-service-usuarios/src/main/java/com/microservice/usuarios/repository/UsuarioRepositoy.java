c060735a43c67a7783c2d214e45113424dbc2be0
alefo28 <alefo28@gmail.com>
2023-11-29 13:35:56 -0600

    Creando Repository Usuarios

---
+++ springboot-service-usuarios/src/main/java/com/microservice/usuarios/repository/UsuarioRepositoy.java
@@ -0,0 +1,21 @@
+package com.microservice.usuarios.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.microservice.usuarios.entity.Usuario;
+
+public interface UsuarioRepositoy extends PagingAndSortingRepository<Usuario, Long>{
+
+	//select u from Usuario u where u.ysername = ?1
+	public Usuario findByUsername(String username);
+
+	//select u from Usuario u where u.username = ?1 and u.email = ?2
+	public Usuario findByUsernameAndEmail(String username, String email);
+
+	@Query(value = "select u from Usuario u where u.ysername = ?1")
+	public Usuario obtenerPorUsername(String username);
+
+	@Query(value = "select u from Usuario u where u.username = ?1 and u.email = ?2")
+	public Usuario obtenerPorUsernameYEmail(String username, String email);
+}
