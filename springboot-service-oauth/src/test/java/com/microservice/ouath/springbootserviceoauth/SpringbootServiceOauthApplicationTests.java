674792ec0f9af406c09fa96b75bbd3965dafbebd
alefo28 <alefo28@gmail.com>
2023-11-29 21:04:31 -0600

    Creando Servicio OAuth

---
+++ springboot-service-oauth/src/test/java/com/microservice/ouath/springbootserviceoauth/SpringbootServiceOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.ouath.springbootserviceoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
