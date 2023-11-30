085190e6eebaa1c6f8c6d6d085debcd71178e0c7
alefo28 <alefo28@gmail.com>
2023-11-29 21:12:09 -0600

    Creando Interfaz Feign

--- springboot-service-oauth/src/main/java/com/microservice/ouath/springbootserviceoauth/SpringbootServiceOauthApplication.java
+++ springboot-service-oauth/src/main/java/com/microservice/ouath/springbootserviceoauth/SpringbootServiceOauthApplication.java
@@ -2,8 +2,12 @@ package com.microservice.ouath.springbootserviceoauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class SpringbootServiceOauthApplication {
 
 	public static void main(String[] args) {
