fc81ec0ec821053d583e24b29ffb912d371fe96c
alefo28 <alefo28@gmail.com>
2023-11-29 20:05:25 -0600

    Exponiendo ID's de Entidades

---
+++ springboot-service-usuarios/src/main/java/com/microservice/usuarios/springbootserviceusuarios/RepositoryConfig.java
@@ -0,0 +1,17 @@
+package com.microservice.usuarios.springbootserviceusuarios;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.microservice.usuarios.entity.Role;
+import com.microservice.usuarios.entity.Usuario;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		config.exposeIdsFor(Usuario.class, Role.class);
+	}
+}
